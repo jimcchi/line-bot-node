c16b7070469301cafe82927c6408b17005445b0d
jimcchi <jimcchi@hotmail.com.tw>
2024-12-09 13:44:58 +0800

    ✨ feat: v1.0.0 test

---
+++ api/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { reply } from '../services/line.js';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.post('/webhook', async (req, res) => {
+  const events = req.body.events || [];
+  const replies = events
+    .filter(({ type }) => type === 'message')
+    .map(({ replyToken, message }) => reply({
+      replyToken,
+      messages: [
+        {
+          type: 'text',
+          text: message.text,
+        },
+      ],
+    }));
+  await Promise.all(replies);
+  res.sendStatus(200);
+});
+
+export default app;
